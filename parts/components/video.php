32ec86e7c71d774ee2178e2ff8d3cf0dff0185ba
brillantagency <dev@brillant.lu>
2026-01-15 16:40:13 +0100

    fix cta block accordeons

--- parts/components/video.php
+++ parts/components/video.php
@@ -1,11 +1,7 @@
 <?php if($media): ?>
 <div class="component_video">
     <?php if ($video_upload): ?>
-        <iframe class="video"
-            src="https://www.youtube.com/embed/<?php echo $video_upload; ?>?rel=0&modestbranding=1&<?php echo $video_autoplay ? 'autoplay=1&mute=1&' : '' ?><?php echo $video_loop ? 'loop=1&playlist=' . $video_upload . '&' : ''; ?><?php echo $video_control ? 'controls=1' : 'controls=0'; ?>" 
-            allow="autoplay; encrypted-media" 
-            allowfullscreen>
-        </iframe>
+        <iframe class="video" src="https://www.youtube.com/embed/<?php echo esc_attr($video_upload); ?>?rel=0&modestbranding=1<?php echo $video_autoplay ? '&autoplay=1&mute=1' : ''; ?><?php echo $video_loop ? '&loop=1&playlist=' . esc_attr($video_upload) : ''; ?>&controls=<?php echo $video_control ? 1 : 0; ?>" allow="autoplay; encrypted-media" allowfullscreen></iframe>
     <?php elseif($video_embed): ?>
         <video class="video"
             src="<?php echo esc_url($video_embed['url']); ?>"
